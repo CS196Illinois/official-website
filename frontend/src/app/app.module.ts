42a4114c4a5762fb570f34c42d18b365a4d071ed
albearli <yiliang6@illinois.edu>
2020-05-25 16:12:51 -0500

    Add main pages and routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ResourcesComponent } from './pages/resources/resources.component';
+import { StaffComponent } from './pages/staff/staff.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'resources', component: ResourcesComponent },
+  { path: 'staff', component: StaffComponent},
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    HomeComponent,
+    ResourcesComponent,
+    StaffComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      routes
+      ),
   ],
   providers: [],
   bootstrap: [AppComponent]
