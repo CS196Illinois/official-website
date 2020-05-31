f6af4a43805806e8ecad2145ea2c70aed1ed2e24
albearli <yiliang6@illinois.edu>
2020-05-30 20:47:31 -0500

    Add fontawesome package to project

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ResourcesComponent } from './pages/resources/resources.component';
 import { StaffComponent } from './pages/staff/staff.component';
+import { LecturesTableComponent } from './components/lectures-table/lectures-table.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -14,20 +16,21 @@ const routes: Routes = [
   { path: 'staff', component: StaffComponent},
 ];
 
-
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     HomeComponent,
     ResourcesComponent,
-    StaffComponent
+    StaffComponent,
+    LecturesTableComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(
       routes
-      ),
+    ),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
